7a024ba7768795bf17bf607f3bd479f0d34ddec3
Felipe Castelhano <felipcastelhanogh@gmail.com>
2025-05-19 15:49:13 -0300

    Enviando arquivos iniciais

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+// @Post
+app.post('/usuarios', (req, res) => {
+    
+    users.push(req.body)
+
+    res.send('Ok, aqui deu certo')
+
+})
+
+// @Get
+app.get('/usuarios', (req, res) => {
+    res.json(users)
+})
+
+app.listen(3000)
