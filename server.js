734cc9218f24ecbb15004dfe217619e597b553b3
Felipe Castelhano <felipcastelhanogh@gmail.com>
2025-05-19 17:01:33 -0300

    Criando CRUD com prisma

--- server.js
+++ server.js
@@ -1,22 +1,60 @@
 import express from 'express'
+import { PrismaClient } from './generated/prisma/index.js'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = []
-
 // @Post
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
     
-    users.push(req.body)
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
 
     res.status(201).json(req.body)
 
 })
 
-// @Get
-app.get('/usuarios', (req, res) => {
+// @Get todos os usuarios
+app.get('/usuarios', async (req, res) => {
+
+   const users = await prisma.user.findMany()
+
     res.status(200).json(users)
+})
+
+// Put pelo id
+app.put('/usuarios/:id', async (req, res) => {
+    
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+
+})
+
+app.delete('/usuarios/:id', async (req, res)=>{
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+        res.status(200).json({message: 'Usuário deletado com sucesso'})
+
 })
 
 app.listen(3000)
