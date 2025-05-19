9f9b3457a7e2ab0fdb7b3f38bbf8e8a91316456d
Felipe Castelhano <felipcastelhanogh@gmail.com>
2025-05-19 17:13:45 -0300

    Criando filtro query params no GET users

--- server.js
+++ server.js
@@ -24,7 +24,20 @@ app.post('/usuarios', async (req, res) => {
 // @Get todos os usuarios
 app.get('/usuarios', async (req, res) => {
 
-   const users = await prisma.user.findMany()
+   let users = []
+
+   if (req.query){
+    users = await prisma.user.findMany({
+        where: {
+            name: req.query.name,
+            email: req.query.email,
+            age: req.query.age
+        }
+    })
+   } else {
+    const users = await prisma.user.findMany()
+   }
+    
 
     res.status(200).json(users)
 })
