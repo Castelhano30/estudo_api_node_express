bc9af3f7f3fd5543fe850ff57ab30cf25d7c93ce
Felipe Castelhano <felipcastelhanogh@gmail.com>
2025-05-20 00:40:31 -0300

    instalando cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import { PrismaClient } from './generated/prisma/index.js'
 
 const prisma = new PrismaClient()
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 // @Post
